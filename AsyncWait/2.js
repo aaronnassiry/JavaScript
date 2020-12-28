cb095b50d83f81eceb0a606a7ab4746ad5d5ade8
Aaron Nassiry <anassiry@salesforce.com>
2020-12-27 21:49:55 -0800

    JavaScript classes

--- AsyncWait/2.js
+++ AsyncWait/2.js
@@ -1,49 +1,20 @@
-const random = () => {
-    if (1 === 2){
-        return Promise.resolve(Math.random());
-    }
-}
-const useRandom = async() => {
-    try {
-        const first = await random();
-        const second = await random();
-        const third = await random();
-        console.log(`first: ${first} second: ${second} third: ${third}`);
-    } catch (err) {
-        console.error("Error");
-    }
-}
-
-async function test(){
+async function test() {
     return new Promise((resolve, reject) => {
-    let a = 1 + 2;
-    if (a === 2){
-        resolve("Success");
-    }
-    else{
-        reject("Failed");
-    }
+        let a = 1 + 1;
+        if (a === 3) {
+            resolve("Success");
+        }
+        else {
+            reject("Failed");
+        }
+    });
+}
+
+test().then((message) => {
+    console.log("This is the resolve message: " + message);
+}).catch((message) => {
+    console.log("This is the reject message:  " + message);
+}).catch((error) => {
+    throw new Error(error);
+    process.exitCode = 1;
 });
-}
-
-// test().then((message) => {
-//     console.log("This is the resolve message: " + message);
-// }).catch((message) => {
-//     console.log("This is the reject message:  " + message);
-// }).catch((error) => {
-//     throw new Error(error);
-//     process.exitCode = 1;
-// });
-
-async function useTest(){
-    console.log("Hi");
-    try{
-        console.log("This is the resolve message: " + await test());
-    } catch (error){
-        console.log("This is the reject message:  " + await test());
-    }
-}
-
-useRandom();
-//useTest();
-
