aa05546f71e860a60b959a66b6aee0cc7e84c607
aaronnassiry <hmnassiry@gmail.com>
2020-12-29 12:49:05 -0800

    convert then catch to async and await

--- AsyncWait/2.js
+++ AsyncWait/2.js
@@ -1,8 +1,9 @@
 async function test() {
     return new Promise((resolve, reject) => {
         let a = 1 + 1;
-        if (a === 3) {
-            resolve("Success");
+        if (a === 2) {
+             console.log("Success");
+             resolve("Success");
         }
         else {
             reject("Failed");
@@ -10,11 +11,33 @@ async function test() {
     });
 }
 
-test().then((message) => {
-    console.log("This is the resolve message: " + message);
-}).catch((message) => {
-    console.log("This is the reject message:  " + message);
-}).catch((error) => {
-    throw new Error(error);
-    process.exitCode = 1;
-});
+async function validate(){
+    try {
+       await test();
+    } catch (error){
+        console.error(error);
+    }
+}
+
+validate();
+
+// async function test() {
+//     return new Promise((resolve, reject) => {
+//         let a = 1 + 1;
+//         if (a === 3) {
+//             resolve("Success");
+//         }
+//         else {
+//             reject("Failed");
+//         }
+//     });
+// }
+
+// test().then((message) => {
+//     console.log("This is the resolve message: " + message);
+// }).catch((message) => {
+//     console.log("This is the reject message:  " + message);
+// }).catch((error) => {
+//     throw new Error(error);
+//     process.exitCode = 1;
+// });
